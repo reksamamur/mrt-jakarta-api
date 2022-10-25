85daff22ad2c2355f9e681e6294f192c29dd00c5
reksamur <reksa.kuana9@gmail.com>
2022-10-25 14:21:47 +0700

    Update redis client

--- src/MRTclient/redis/client/client.ts
+++ src/MRTclient/redis/client/client.ts
@@ -1,14 +1,11 @@
 import { createClient, RedisClientType } from "redis";
-import { Client } from "redis-om";
 import dotenv from "dotenv";
 dotenv.config();
 
 export class ClientRedis {
-  public initRedisOM: Promise<Client>;
   public initRedis: RedisClientType;
 
   constructor() {
-    this.initRedisOM = this.openOM();
     this.initRedis = createClient({
       url: `${process.env.URL_REDIS}`,
     });
@@ -18,13 +15,5 @@ export class ClientRedis {
     this.initRedis.on("connect", () => {
       console.log(`Redis connection established`);
     });
-  }
-
-  /**
-   * init redis om
-   */
-  private async openOM() {
-    const client = await new Client().open(`${process.env.URL_REDIS}`);
-    return client;
   }
 }
